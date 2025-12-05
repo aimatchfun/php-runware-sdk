0552be3bbbda1977a2a24a6869a6053594cc6dfa
Diego Avelar <diego@aimatch.fun>
2025-12-05 18:53:22 -0300

    chore: atualiza para versão 1.1.0

--- src/ModelAir.php
+++ src/ModelAir.php
@@ -4,6 +4,14 @@ namespace AiMatchFun\PhpRunwareSDK;
 
 enum ModelAir: string
 {
+    case WAI_ANIME_NSFW_PONYXL = 'civitai:404154@931577';
+    case HENTAI_BASIC = 'civitai:282341@826474';
+    case KLINGUM_PONY = 'civitai:1250172@1414779';
+    case PREFECT_PONY_V6 = 'aimatch:439889@2114187';
+    case AUTISM_MIX = 'civitai:288584@324524';
+    case PREFECT_PONY_V4 = 'civitai:439889@1047139';
+    case PONY_V6 = 'civitai:257749@298112';
+    case PONY_DIFFUSION = 'runware:101@1';
     case PONY_REALISM = 'civitai:372465@534642';
     case REAL_DREAM_SDXL_PONY_14 = 'civitai:153568@1308507';
     case REAL_DREAM_SDXL_PONY_15 = 'civitai:153568@1376263';
@@ -12,6 +20,7 @@ enum ModelAir: string
     case REAL_DREAM_SDXL_PONY_11 = 'civitai:153568@750893';
     case REAL_DREAM_SDXL_PONY_9 = 'civitai:153568@665047';
     case VOLENDIR_PONY_CINEMATIC = 'civitai:723371@812199';
+    case UNTITLED_PONY = 'aimatch:99999@1';
     case PONY_REALISM_2_2 = 'civitai:372465@914390';
     case STELLARATOR_MIX = 'civitai:980168@1233636';
     case GODDESS_OF_REALISM = 'civitai:212737@714766';
@@ -19,4 +28,7 @@ enum ModelAir: string
     case PONY_REALISM_2_2_4S = 'civitai:372465@987210';
     case FLUX = 'civitai:618692@691639';
     case CYBERREALISTIC_PONY_V8 = 'civitai:443821@1346181';
+    case HASSAKU_XL_ILLUSTRIOUS = 'civitai:140272@325904';
+    case ILLUSTRIOUS_XL = 'x:42@889818';
+    case BABES_BY_STABLE_YOGI_V5 = 'aimatch:1134825@2044007';
 }
